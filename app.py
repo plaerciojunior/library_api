ec22db354db8dc1e883f965695437b22d7d0fd76
plaerciojunior <plaerciojunior@hotmail.com>
2024-06-22 19:13:57 -0300

    Add Get ID

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #Rota para criação do livro
 @app.route('/', methods=['POST'])
 @db_session
-def get_all():
+def create_book():
     data = request.json
     name = data['name']
     author = data['author']
@@ -17,6 +17,20 @@ def get_all():
         {"Status": "Created"})
 
 #Rota para Ler um livro de acordo com seu id
+@app.route('/<string:name2>', methods=['GET'])
+@db_session
+def get_byid(name2):
+    book = Livro.get(id=name2)
+    
+    return jsonify(
+        {"Status": "Response",
+        "Id": book.id,
+        "Name": book.name,
+        "Author": book.author}
+        )
+    
+
+
 
 
 app.run(debug=True)
